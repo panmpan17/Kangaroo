c32a16f9b92c2e43ca28634cc4d39ae6e09cfebf
Michael Pan <panmpan@gmail.com>
2020-08-03 11:18:45 +0800

    Simple

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-from server import Server
+import os
+import jinja2
+import cherrypy
 
-server = Server("sqlite:///kangaroo.db")
-server.run()
+
+dirname = os.path.join(os.path.dirname(__file__), "server/template")
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(dirname))
+
+
+class App:
+    @staticmethod
+    def render(src, **params):
+        return jinja_env.get_template(src).render(params)
+
+    @cherrypy.expose  # function 名字跟 URL 有關
+    def index(self, **data):  # index = "/"
+        return "Index"
+        # if cherrypy.request.method == "GET":
+        #     return self.render("index.html", login=True)
+
+    @cherrypy.expose
+    def url_al(self):
+        return "url"
+
+
+if __name__ == "__main__":
+    cherrypy.quickstart(App(), "/expose/")
