57a926e4563dc546127fab8e1d9ea936b71b85ac
Michael Pan <panmpan@gmail.com>
2020-08-06 17:15:51 +0800

    Cookie

--- main.py
+++ main.py
@@ -13,17 +13,40 @@ class App:
         return jinja_env.get_template(src).render(params)
 
     @cherrypy.expose  # function 名字跟 URL 有關
-    def index(self, **data):  # index = "/"
+    def index(self, login_id=None):  # index = "/"
         if cherrypy.request.method == "GET":
-            return self.render('index.html')
+            login_cookie = cherrypy.request.cookie.get("login_id")
+            password_cookie = cherrypy.request.cookie.get("password")
+
+            if login_cookie is not None:
+                return self.render(
+                    "index.html",
+                    login_id=login_cookie.value)
+
+            return self.render("index.html")
         
         elif cherrypy.request.method == "POST":
-            return str(data)
+            cherrypy.response.cookie["login_id"] = login_id
+            cherrypy.response.cookie["login_id"]["path"] = "/"
+            cherrypy.response.cookie["login_id"]["max-age"] = 3600
+
+            raise cherrypy.HTTPRedirect("/")
+    
+    @cherrypy.expose
+    def logout(self):
+        if "login_id" in cherrypy.request.cookie.keys():
+            cherrypy.response.cookie["login_id"] = ""
+            cherrypy.response.cookie["login_id"]["expires"] = 0
+            print(cherrypy.response.cookie["login_id"])
+
+        raise cherrypy.HTTPRedirect("/")
     
     @cherrypy.expose
     def login(self, **kwargs):
-        return cherrypy.request.method
-        return str(kwargs)
+        cherrypy.response.cookie["test"] = "test"
+        cherrypy.response.cookie["test"]["path"] = "/login"
+        cherrypy.response.cookie["test"]["max-age"] = 3600
+        return "Cookies!"
 
     @cherrypy.expose
     def url_al(self):
@@ -32,7 +55,7 @@ class App:
 
 if __name__ == "__main__":
     cherrypy.quickstart(App(), "/", {
-        "/images": {
+        "/": {
             "tools.staticdir.on": True,
             "tools.staticdir.dir": os.path.join(os.getcwd(), "server", "static"),
         }
